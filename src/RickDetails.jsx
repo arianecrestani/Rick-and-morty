df3432c8b003465ce515899f0d7e24a4bf3e9834
arianecrestani <ariane.mrv@gmail.com>
2023-03-15 14:38:32 +0100

    add material ui

---
+++ src/RickDetails.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { useState } from "react";
+// import Typography from "@mui/material/Typography";
+// import Box from "@mui/material/Box";
+// import Modal from "@mui/material/Modal";
+// import Button from "@mui/material/Button";
+
+// export const RickDetails = () => {
+//   const [openCart, setOpenCart] = useState(true);
+//   const [closeCart, setCloseCart] = useState(false);
+
+
+
+
+
+//   return (
+//     <>
+ 
+//     </>
+//   );
+// };
