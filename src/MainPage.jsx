ab12c0168be3a3bb389925f605d04a458adab333
arianecrestani <ariane.mrv@gmail.com>
2023-03-15 20:56:12 +0100

    reuse component

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
+import { RickDetails } from "./RickDetails";
 
 export default function MainPage() {
-  const [RickMortyDate, setRickMortyDate] = useState();
+  const [rickMortyDate, setRickMortyDate] = useState();
   const [openCart, setOpenCart] = useState(false);
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
   const handleOpen = () => {
     setOpenCart(true);
   };
@@ -41,31 +29,17 @@ export default function MainPage() {
 
   return (
     <>
-      <Modal
-        open={openCart}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
       <Box sx={{ flexGrow: 2 }}>
         <Grid
           container
           spacing={{ sm: 2, sm: 5, md: 8 }}
           columns={{ sm: 8, md: 12 }}
         >
-          {RickMortyDate &&
-            RickMortyDate.map(({ image }, index) => (
+          {rickMortyDate &&
+            rickMortyDate.map(({ image }, index) => (
               <Grid key={index}>
                 <img src={image} onClick={() => handleOpen(true)} />
+                <RickDetails open={openCart} onClose={handleClose} />
               </Grid>
             ))}
         </Grid>
--- src/RickDetails.jsx
+++ src/RickDetails.jsx
@@ -1,21 +1,37 @@
-// import React from "react";
-// import { useState } from "react";
-// import Typography from "@mui/material/Typography";
-// import Box from "@mui/material/Box";
-// import Modal from "@mui/material/Modal";
-// import Button from "@mui/material/Button";
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 
-// export const RickDetails = () => {
-//   const [openCart, setOpenCart] = useState(true);
-//   const [closeCart, setCloseCart] = useState(false);
+export const RickDetails = ({ open, onClose }) => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
-
-
-
-
-//   return (
-//     <>
- 
-//     </>
-//   );
-// };
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </>
+  );
+};
